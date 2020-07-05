cbfcd804aa38be0b1276d36243d1f1b390b0fe39
João Carlos <joaocamargo812@gmail.com>
2020-07-05 01:15:01 -0300

    Add Github Actions

--- createApp.tf
+++ createApp.tf
@@ -1,20 +1,16 @@
 provider "heroku" {
   version = "~> 2.0"
-  email   = "joao-mostela@hotmail.com"
-  api_key = "ca1d2283-6668-463f-aa25-093df3fa9f40"
+  email   = var.email
+  api_key = var.api_key
 }
-# USADO PARA SABER SE EXISTE UMA APLICAÇAO COM O MESMO NOME
-#data "heroku_app" "default" {
-#  name   = "my-pizza-mostelacamargo"
-#}
 resource "heroku_app" "mypizza" {
   name   = "my-pizza-mostelacamargo"
   region = "us"
 
   config_vars = {
-    NOMENEGOCIO= "Pizzaria do Joao"
-    DATABASE_ID="1c0fb0bd-5f0d-4274-b411-1e7e0e1e8e91"
-    DATABASE_TOKEN="3804359e-6fe9-4831-834a-a3ec271fa170"
+    NOMENEGOCIO    = "Pizzaria do Joao"
+    DATABASE_ID    = "1c0fb0bd-5f0d-4274-b411-1e7e0e1e8e91"
+    DATABASE_TOKEN = "3804359e-6fe9-4831-834a-a3ec271fa170"
   }
 }
 
@@ -31,6 +27,6 @@ output "mypizza_app_url" {
   value = "https://heroku_app.mypizza.name.herokuapp.com/produto"
 }
 
-output "saida_do_git"{
+output "saida_do_git" {
   value = "git clone heroku_app.mypizza.git_url"
 }
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "email" {
+}
+variable "api_key" {
+}
